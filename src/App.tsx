c86ef31f532fa54e80cd9defa21824fd26067cdb
Gustavo Azevedo <gustavonovais@febracis.com.br>
2024-01-06 02:43:36 -0300

    Rota das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
-import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { Rounter } from './router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Rounter />
+      </BrowserRouter>
       <GlobalStyle />
-      <h1>Pomodoro</h1>
-      <Button text="Enviar" />
-      <Button text="Error" variant="error" />
-      <Button text="Warning" variant="warning" />
     </ThemeProvider>
   )
 }
